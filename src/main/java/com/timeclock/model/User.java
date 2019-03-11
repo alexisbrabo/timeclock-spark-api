e9626cfd8e61ab7acfdefaf5ef2c035afca72621
alexisbrabo <alexis_brabo@hotmail.com>
2019-03-11 20:53:38 -0300

    Model correction.

--- src/main/java/com/timeclock/model/User.java
+++ src/main/java/com/timeclock/model/User.java
@@ -1,5 +1,6 @@
 package com.timeclock.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@Column(nullable = false, unique = true)
 	private String name;
 
 	public Integer getId() {
